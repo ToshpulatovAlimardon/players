30f73ad31e3112db7e651b773b118424437bdca4
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-07-31 11:36:40 +0500

    changed stringMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,24 @@
-import { combineReducers, compose, legacy_createStore } from "redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  legacy_createStore,
+} from "redux";
 import players from "../reducers/players";
 import filters from "../reducers/filters";
 
-const enhancer =
-  (createStore) =>
-  (...args) => {
-    const store = createStore(...args);
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === "string") {
+    return next({ type: action });
+  }
 
-    const oldDispatch = store.dispatch;
-
-    store.dispatch = (action) => {
-      if (typeof action === "string") {
-        return oldDispatch({
-          type: action,
-        });
-      }
-
-      return oldDispatch(action);
-    };
-
-    return store;
-  };
+  return next(action);
+};
 
 const store = legacy_createStore(
   combineReducers({ players, filters }),
   compose(
-    enhancer,
+    applyMiddleware(stringMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
